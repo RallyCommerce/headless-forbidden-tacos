19ef1205ca17e9adb1723fa6fd03403980af5499
Chris Collinsworth <cmcollin41@gmail.com>
2023-02-22 11:24:05 -0700

    setup nextjs 13

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/chrisc/Desktop/Rally/headless-forbidden-tacos/app/page": [
+      "/Users/chrisc/Desktop/Rally/headless-forbidden-tacos/app/page.module.css",
+      "/Users/chrisc/Desktop/Rally/headless-forbidden-tacos/node_modules/@next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/chrisc/Desktop/Rally/headless-forbidden-tacos/app/globals.css"
+    ]
+  },
+  "/Users/chrisc/Desktop/Rally/headless-forbidden-tacos/app/page.tsx": [
+    "/Users/chrisc/Desktop/Rally/headless-forbidden-tacos/app/page.module.css",
+    "/Users/chrisc/Desktop/Rally/headless-forbidden-tacos/node_modules/@next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ],
+  "/Users/chrisc/Desktop/Rally/headless-forbidden-tacos/app/layout.tsx": [
+    "/Users/chrisc/Desktop/Rally/headless-forbidden-tacos/app/globals.css"
+  ]
+}
